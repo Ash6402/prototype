2a458f0cef131c03d5fae44f887a6f81534dbed1
Aashir Razzaq <aashir.razzaq10@gmail.com>
2024-03-24 15:32:55 +0500

    implemented a 2D blend tree with 7 states for moving in different directions

--- Assets/AnimationStateController.cs
+++ Assets/AnimationStateController.cs
@@ -10,9 +10,13 @@ public class AnimationStateController : MonoBehaviour
     public Animator animator;
     private InputAction _moveAction;
     private InputAction _runAction;
-    private readonly int _walk = Animator.StringToHash("isWalking");
-    private readonly int _run = Animator.StringToHash("isRunning");
+    // private readonly int _walk = Animator.StringToHash("isWalking");
+    // private readonly int _run = Animator.StringToHash("isRunning");
 
+    private readonly int _x = Animator.StringToHash("Velocity X");
+    private readonly int _z = Animator.StringToHash("Velocity Z");
+    private float _xVelocity;
+    private float _zVelocity;
     private void OnEnable()
     {
         controls = new CharacterControls();
@@ -36,8 +40,11 @@ public class AnimationStateController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        // var walk = _moveAction.ReadValue<Vector2>().y;
-        // var run = _runAction.ReadValue<float>();
+        var moveDirection = _moveAction.ReadValue<Vector2>();
+        var run = _runAction.ReadValue<float>().Equals(1);
+        
+        _controlAnimation(moveDirection, run);
+        
         // if(walk.Equals(1) && run.Equals(1)){
         //     animator.SetBool(_run, true);
         // }else if(walk.Equals(1) && !run.Equals(1))
@@ -50,5 +57,23 @@ public class AnimationStateController : MonoBehaviour
         //     animator.SetBool(_run, false);
         //     animator.SetBool(_walk, false);
         // }
+    }
+
+    private void _controlAnimation(Vector2 moveDirection, bool isRunning)
+    {
+        var x = moveDirection.x;
+        var z = moveDirection.y;
+        
+        // on double direction input (e.g. w + a, w + d ...) x doesn't reach 1 or -1 so I am forcing it or the animations are slow.
+        x = x > 0 ? 1 : x;
+        x = x < 0 ? -1 : x;
+        z = z > 0 ? 1 : z;
+        z = z < 0 ? -1 : z;
+        
+        var currentX = animator.GetFloat(_x);
+        var currentZ = animator.GetFloat(_z);
+        var upperBound = isRunning ? 2 : 0.5f;
+        animator.SetFloat(_x, Mathf.SmoothDamp(currentX, (upperBound * x), ref _xVelocity, 1.5f * Time.deltaTime));
+        animator.SetFloat(_z, Mathf.SmoothDamp(currentZ, (upperBound * z), ref _zVelocity, 1.5f * Time.deltaTime));
     }
 }
